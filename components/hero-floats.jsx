a245b70c2aa5474d4c93ffe79e527f11c9d5cfb7
OGEmanuel <1blacksanta416@gmail.com>
2023-04-10 11:13:43 +0100

    hero responsiveness added

--- components/hero-floats.jsx
+++ components/hero-floats.jsx
@@ -1,11 +1,11 @@
 const HeroFloats = props => {
   const { p, span, className } = props;
   const classes =
-    'absolute bg-white text-center rounded-[16px] px-5 py-2.5 z-20 ' +
+    'absolute bg-white text-center rounded-[16px] px-5 z-20 flex flex-col ' +
     className;
   return (
     <div className={classes}>
-      <p className="font-extrabold md:font-bold text-2xl md:text-[2rem] text-accent-2 font-mont">
+      <p className="font-extrabold text-2xl text-accent-2 font-mont md:font-bold md:text-[2rem]">
         {p}
       </p>
       <span className="text-xs md:text-base">{span}</span>
--- components/hero-illustration.jsx
+++ components/hero-illustration.jsx
@@ -11,27 +11,27 @@ const HeroIllustration = props => {
       <Image
         src={woman}
         alt="woman"
-        className="top-[2.6rem] md:top-[5.2rem] left-0 bg-no-repeat bg-contain z-40 relative max-w-[37rem] w-[100%] md:w-[29rem]"
+        className="top-[2.6rem] left-0 bg-no-repeat bg-contain z-40 relative max-w-[37rem] w-[100%] md:w-[25rem] md:top-[5.9rem] xl:top-[5.2rem] xl:w-[29rem]"
       />
       <Image
         src={background}
         alt="background"
-        className="absolute top-0 right-0 z-[10] max-w-[25rem] w-[71.5%] md:w-[22rem]"
+        className="absolute top-0 right-0 z-10 max-w-[25rem] w-[71.5%] md:w-[20rem] xl:w-[22rem]"
       />
       <HeroFloats
         p="100k+"
         span="Total assessments"
-        className="-top-[1rem] md:top-0 left-[2rem] shadow-float"
+        className="-top-[1rem] left-[2rem] shadow-float gap-4 py-2.5 md:top-0"
       />
       <HeroFloats
         p="85%"
         span="Accuracy"
-        className="top-[0.5rem] md:top-[1rem] right-0 md:right-[1rem]"
+        className="top-[0.5rem] right-0 gap-2.5 py-2.5 md:top-[1rem] md:right-[1rem]"
       />
       <HeroFloats
         p="500+"
         span="Daily tests"
-        className="-bottom-[1rem] md:-bottom-[2rem] left-[2rem] z-50"
+        className="-bottom-[1rem] left-[2.5rem] z-[60] gap-2.5 py-2.5 md:bottom-[0.5rem] md:left-[0.5rem] xl:left-[2.5rem] xl:-bottom-[1.2rem]"
       />
     </div>
   );
