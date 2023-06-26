5e1cef023b4166ee8bc7afbbc739824d8300a62c
Hikmot-O <hikmotokunola@gmail.com>
2023-06-26 13:44:43 +0100

    minor fix

--- components/home-page/Qualities.jsx
+++ components/home-page/Qualities.jsx
@@ -5,7 +5,7 @@ import techskillIllustration from '../../public/images/techskills-Illustration.p
 
 const Qualities = () => {
   return (
-    <section className="border-t bg-techskills-pattern bg-cover px-20 pb-[100px] mt-[100px] pt-[100px] flex flex-col-reverse md:flex-row justify-between items-center">
+    <section className="border-t bg-techskills-pattern bg-cover px-5 lg:px-20 sm:px-10 pb-[100px] mt-[100px] pt-[100px] flex flex-col-reverse md:flex-row justify-between items-center">
       {/* left side */}
       <div className="flex">
         <div className=" flex flex-col gap-[88px] mt-[100px] items-end">
@@ -24,17 +24,17 @@ const Qualities = () => {
       </div>
       {/* right side */}
       <div className="relative">
-        <Image className='w-[200px] md:w-auto absolute z-[-1] right-[40px] top-[-80px]' src={techskillIllustration} alt='Illustration'/>
-        <p className="text-[#048481]">Qualities</p>
-        <h4 className="font-[700] text-[40px] text-[#292627]">Tech Skills</h4>
-        <p className="text-lg leading-6 mt-4 text-[#5D5658] max-w-[600px]">
+        <Image className='w-[200px] h-[200px] md:h-auto md:w-auto absolute z-[-1] right-5 md:right-[40px] top-[5px] md:top-[-80px]' src={techskillIllustration} alt='Illustration'/>
+        <p className="text-[#048481] text:xs md:text-base">Qualities</p>
+        <h4 className="font-[700] text-2xl leading-9 md:text-[40px] md:leading-[56px] text-[#292627]">Tech Skills</h4>
+        <p className="text-sm md:text-lg leading-6 mt-4 text-[#5D5658] md:max-w-[600px]">
           What exactly makes one become good in tech? From creativity to problem
           solving, learn about the qualities that will help you excel in your
           chosen field
         </p>
         <div className="mt-10 flex items- gap-3">
-            <p className="text-[#F58025]">Learn More</p>
-            <Image src={arrow} alt='Arrow right Svg' />
+            <p className="text-[#F58025] text-sm font-[700] md:font-[400] md:text-base">Learn more</p>
+            <Image className='w-5 h-5 md:w-6 md:h-6' src={arrow} alt='Arrow right Svg' />
         </div>
       </div>
     </section>
