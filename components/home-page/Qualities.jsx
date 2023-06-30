7b1f3837cc99e598a55da39fa71b8da1f993ec57
Hikmot-O <hikmotokunola@gmail.com>
2023-06-30 15:36:25 +0100

    skills media query fix

--- components/home-page/Qualities.jsx
+++ components/home-page/Qualities.jsx
@@ -5,7 +5,7 @@ import techskillIllustration from '../../public/images/techskills-Illustration.p
 
 const Qualities = () => {
   return (
-    <section className="border-t bg-techskills-pattern bg-cover px-5 lg:px-20 sm:px-10 pb-[100px] mt-[100px] pt-[100px] flex flex-col-reverse md:flex-row justify-between items-center">
+    <section className="border-t bg-techskills-pattern bg-cover px-5 xl:px-20 sm:px-10 pb-[100px] lg:mt-[100px] pt-[120px] lg:pt-[100px] flex flex-col-reverse lg:flex-row justify-between items-center">
       {/* left side */}
       <div className="flex">
         <div className=" flex flex-col gap-[88px] mt-[100px] items-end">
@@ -14,7 +14,7 @@ const Qualities = () => {
           <div className="qualities">Curiosity</div>
           <div className="qualities">Persistence</div>
         </div>
-        <div className="h-[590px] w-2.5 bg-[#292627] rounded-lg"></div>
+        <div className="h-[590px] w-2 lg:w-2.5 bg-[#292627] rounded-lg"></div>
         <div className="flex flex-col gap-[88px] mt-10">
           <div className="qualities_right">Problem solving</div>
           <div className="qualities_right">Communication</div>
@@ -25,9 +25,9 @@ const Qualities = () => {
       {/* right side */}
       <div className="relative">
         <Image className='w-[200px] h-[200px] md:h-auto md:w-auto absolute z-[-1] right-5 md:right-[40px] top-[5px] md:top-[-80px]' src={techskillIllustration} alt='Illustration'/>
-        <p className="text-[#048481] text:xs md:text-base">Qualities</p>
-        <h4 className="font-[700] text-2xl leading-9 md:text-[40px] md:leading-[56px] text-[#292627]">Tech Skills</h4>
-        <p className="text-sm md:text-lg leading-6 mt-4 text-[#5D5658] md:max-w-[600px]">
+        <p className="text-[#048481] text:xs lg:text-base">Qualities</p>
+        <h4 className="font-[700] text-2xl leading-9 lg:text-[40px] md:leading-[56px] text-[#292627]">Tech Skills</h4>
+        <p className="text-sm lg:text-lg leading-6 mt-4 text-[#5D5658] md:max-w-[600px]">
           What exactly makes one become good in tech? From creativity to problem
           solving, learn about the qualities that will help you excel in your
           chosen field
