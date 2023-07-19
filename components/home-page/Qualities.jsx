7f13c6adf07637b764a1ab60a5fa1a5e5c15aff4
Hikmot-O <hikmotokunola@gmail.com>
2023-07-19 15:35:37 +0100

    fix

--- components/home-page/Qualities.jsx
+++ components/home-page/Qualities.jsx
@@ -5,7 +5,7 @@ import techskillIllustration from '../../public/images/techskills-Illustration.p
 
 const Qualities = () => {
   return (
-    <section className="sm:border-t bg-techskills-pattern bg-cover px-5 xl:px-20 sm:px-10 pb-[100px] lg:mt-[100px] mt-0 pt-[20px] lg:pt-[100px] flex flex-col-reverse md:flex-row justify-between items-center">
+    <section className="sm:border-t bg-techskills-pattern bg-cover px-5 lg:px-10 xl:px-20 pb-[100px] lg:mt-[100px] mt-0 pt-[20px] lg:pt-[100px] flex flex-col-reverse md:flex-row justify-between items-center">
       {/* left side */}
       <div className="flex">
         <div className=" flex flex-col gap-[88px] mt-[100px] items-end">
@@ -24,10 +24,10 @@ const Qualities = () => {
       </div>
       {/* right side */}
       <div className="relative">
-        <Image className='w-[200px] h-[200px] md:h-auto md:w-auto absolute z-[-1] right-5 md:right-[40px] top-[5px] md:top-[-80px]' src={techskillIllustration} alt='Illustration'/>
+        <Image className='w-[200px] h-[200px] md:w-[300px] md:h-[300px] lg:h-auto lg:w-auto absolute z-[-1] right-5 md:right-[40px] top-[5px] md:top-[-80px]' src={techskillIllustration} alt='Illustration'/>
         <p className="text-[#048481] text:xs lg:text-base">Qualities</p>
         <h4 className="font-[700] text-2xl leading-9 lg:text-[40px] md:leading-[56px] text-[#292627]">Tech Skills</h4>
-        <p className="text-sm lg:text-lg leading-6 mt-4 text-[#5D5658] md:max-w-[600px]">
+        <p className="text-sm lg:text-lg leading-6 mt-4 text-[#5D5658] md:w-[400px] lg:w-[600px]">
           What exactly makes one become good in tech? From creativity to problem
           solving, learn about the qualities that will help you excel in your
           chosen field
